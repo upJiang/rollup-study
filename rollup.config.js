0bb3de6e81e4c598916562806141d2134c165d7c
jiangjunfeng <jiangjf04@vanke.com>
2022-04-21 17:22:54 +0800

    feat: 更新rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
 /**
  * @type { import('rollup').RollupOptions }
  */
- const buildOptions = {
-    input: ["src/index.js", "src/util.js"],
-    output: [
-        {
-          dir: "dist/es",
-          format: "esm",
-        },
-        {
-          dir: "dist/cjs",
-          format: "cjs",
-        },
-      ]
-  };
-  
-  export default buildOptions;
+export default {
+  input: ["src/index.js"],
+  output: [
+    {
+      dir: "dist/es",
+      format: "esm",
+    },
+    {
+      dir: "dist/cjs",
+      format: "cjs",
+    },
+  ],
+  // 通过 plugins 参数添加插件
+  plugins: [resolve(), commonjs()],
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import { add } from "./util";
+import { merge } from "lodash";
+console.log(merge);
 console.log(add(1, 2));
